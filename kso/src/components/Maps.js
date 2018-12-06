3a7763f7b29abe959a9fb8218353809ba4dcbab1
Unknown <gilles.labrousse@etu.univ-tlse2.fr>
2018-12-06 23:54:26 +0100

    Ajout localisation

--- kso/src/components/Maps.js
+++ kso/src/components/Maps.js
@@ -8,23 +8,36 @@ var macarte = null;
 
 class Maps extends Component {
 
+  displayCurrentLocation(){
 
+  }
 
   componentDidMount(){
     this.initMap();
+    this.displayCurrentLocation();
   }
 
   initMap() {
-    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-    macarte = L.map('map').setView([lat, lon], 11);
-    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-    // Il est toujours bien de laisser le lien vers la source des données
-      attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-      minZoom: 1,
-      maxZoom: 20
-    }).addTo(macarte);
-    var marker = L.marker([lat, lon]).addTo(macarte);
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition((function (position) {
+            macarte = L.map('map').setView([position.coords.latitude, position.coords.longitude], 11);
+            // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+            L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+            // Il est toujours bien de laisser le lien vers la source des données
+              attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+              minZoom: 1,
+              maxZoom: 20
+            }).addTo(macarte);
+
+            var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(macarte);
+            marker.bindPopup("Ma position :<br> Latitude : " + position.coords.latitude + ',<br>Longitude ' + position.coords.longitude).openPopup();
+        }));
+    } else {
+        alert("Localisation impossible");
+    }
+
   }
 
   render() {
